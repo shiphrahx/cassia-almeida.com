bca5c07a9965e232f387c0193ddd9bcb47ebad6f
Shiphrah <lisa.shiphrah@gmail.com>
2026-05-27 08:07:13 +0100

    feat: add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <ul className="footer-links">
+        <li><a href="https://github.com/shiphrahx" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/cassiaalmeida/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+        <li><a href="https://medium.com/@shiphrah_" target="_blank" rel="noopener noreferrer">Medium</a></li>
+      </ul>
+      <span className="footer-copy">© 2026</span>
+    </footer>
+  );
+}
